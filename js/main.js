9bbeeb3d937b2f7a606cf2b56c2322e714b50ff0
Forz70043 <alfonso.pisicchio@libero.it>
2021-06-16 12:10:27 +0200

    clean js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ const MIN = -99;
 const MAX = 99;
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed',event);
     hideSign();
     setMinusSign();
     addButtons();
@@ -30,24 +29,20 @@ function addButtons(){
 }
 
 function decrement(){
-    console.log("decrement");
+
     if(counter > MIN) counter--;
-    else alert("Non ho implementato il conteggio maggiore di -99");
-    console.log("Counter: ",counter);
+    else alert("I have not implemented the count greater than -99");
     
     if(counter<0){
+        
         showSign();
-        if(counter<-9){
-            console.log("MINORE DI -9");
-            let newCounterValue = Math.abs(counter);
-            splitNumbers(newCounterValue);
-        }
-        else{
-            let newCounterValue = Math.abs(counter);
-            setNumber(newCounterValue);
-        }
+        let newCounterValue = Math.abs(counter);
+
+        if(counter<-9) splitNumbers(newCounterValue);
+        else setNumber(newCounterValue);
     }
     else {
+
         hideSign();
         
         if(counter>=0 && counter<10){
@@ -59,38 +54,31 @@ function decrement(){
 }
 
 function increment(){
-    console.log("INCREMENT");
+
     if(counter < MAX) counter++;
-    else alert("Non ho implementato il conteggio maggiore di 99");
+    else alert("I have not implemented the count greater than 99");
 
     if(counter<0){
-        if(counter<-9){
-            let newCounterValue = Math.abs(counter);
-            splitNumbers(newCounterValue);    
-        }
+        let newCounterValue = Math.abs(counter);
+        
+        if(counter<-9) splitNumbers(newCounterValue);
         else{
             setNumber(0,true);
-            let newCounterValue = Math.abs(counter);
             setNumber(newCounterValue);
         }
     }
     else hideSign();
     
     if(counter<10) setNumber(counter);
-    else {
-        splitNumbers(counter);
-    }
-    console.log("Counter: ", counter);
+    else splitNumbers(counter);
+    
 }
 
 function splitNumbers(number){
-    console.log("Number: ",number);
+
     let stringNumber = number.toString();
-    console.log("String Number: ", stringNumber);
     let tens = stringNumber[0];
     let unit = stringNumber[1];
-    console.log("Tens: ",tens);
-    console.log("Unit: ", unit);
     setNumber(parseInt(tens),true);
     setNumber(parseInt(unit));
 }
@@ -131,142 +119,132 @@ function setNumber(counter,unitTens=false){
 }
 
 function setMinusSign(){
-    let sectionTop = document.getElementById('signNumberSectionTop').style.opacity="0%";
-    let sectionTopRight = document.getElementById('signNumberSectionTopRight').style.opacity="0%";
-    let sectionTopLeft = document.getElementById('signNumberSectionTopLeft').style.opacity="0%";
-    let middleTop = document.getElementById('signNumberMiddleTop').style.opacity="100%";
-    let middleBottom = document.getElementById('signNumberMiddleBottom').style.opacity="100%";
-    let sectionBottomRight = document.getElementById('signNumberSectionBottomRight').style.opacity="0%";
-    let sectionBottomLeft = document.getElementById('signNumberSectionBottomLeft').style.opacity="0%";
-    let sectionBottom = document.getElementById('signNumberSectionBottom').style.opacity="0%";
+    document.getElementById('signNumberSectionTop').style.opacity="0%";
+    document.getElementById('signNumberSectionTopRight').style.opacity="0%";
+    document.getElementById('signNumberSectionTopLeft').style.opacity="0%";
+    document.getElementById('signNumberMiddleTop').style.opacity="100%";
+    document.getElementById('signNumberMiddleBottom').style.opacity="100%";
+    document.getElementById('signNumberSectionBottomRight').style.opacity="0%";
+    document.getElementById('signNumberSectionBottomLeft').style.opacity="0%";
+    document.getElementById('signNumberSectionBottom').style.opacity="0%";
 }
 
 function setZero(unitTens){
     let unit= unitTens ? 'first' : 'second';
-    console.log(unit);
-    let sectionTop = document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
-    let sectionTopRight = document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
-    let sectionTopLeft = document.getElementById(unit+'NumberSectionTopLeft').style.opacity="100%";
-    let middleTop = document.getElementById(unit+'NumberMiddleTop').style.opacity="0%";
-    let middleBottom = document.getElementById(unit+'NumberMiddleBottom').style.opacity="0%";
-    let sectionBottomRight = document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
-    let sectionBottomLeft = document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="100%";
-    let sectionBottom = document.getElementById(unit+'NumberSectionBottom').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopLeft').style.opacity="100%";
+    document.getElementById(unit+'NumberMiddleTop').style.opacity="0%";
+    document.getElementById(unit+'NumberMiddleBottom').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottom').style.opacity="100%";
 }
 
 function setOne(unitTens){
     let unit= unitTens ? 'first' : 'second';
-    console.log(unit);
-    let sectionTop = document.getElementById(unit+'NumberSectionTop').style.opacity="0%";
-    let sectionTopRight = document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
-    let sectionTopLeft = document.getElementById(unit+'NumberSectionTopLeft').style.opacity="0%";
-    let middleTop = document.getElementById(unit+'NumberMiddleTop').style.opacity="0%";
-    let middleBottom = document.getElementById(unit+'NumberMiddleBottom').style.opacity="0%";
-    let sectionBottomRight = document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
-    let sectionBottomLeft = document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="0%";
-    let sectionBottom = document.getElementById(unit+'NumberSectionBottom').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionTop').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopLeft').style.opacity="0%";
+    document.getElementById(unit+'NumberMiddleTop').style.opacity="0%";
+    document.getElementById(unit+'NumberMiddleBottom').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionBottom').style.opacity="0%";
 }
 
 function setTwo(unitTens){
     let unit= unitTens ? 'first' : 'second';
-    console.log(unit);
-    let sectionTop = document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
-    let sectionTopRight = document.getElementById(unit+'NumberSectionTopRight');
-    let sectionTopLeft = document.getElementById(unit+'NumberSectionTopLeft').style.opacity="0%";
-    let middleTop = document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
-    let middleBottom = document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
-    let sectionBottomRight = document.getElementById(unit+'NumberSectionBottomRight').style.opacity="0%";
-    let sectionBottomLeft = document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="100%";
-    let sectionBottom = document.getElementById(unit+'NumberSectionBottom').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopRight');
+    document.getElementById(unit+'NumberSectionTopLeft').style.opacity="0%";
+    document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
+    document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomRight').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottom').style.opacity="100%";
 }
 
 function setThree(unitTens){
     let unit= unitTens ? 'first' : 'second';
-    console.log(unit);
-    let sectionTop = document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
-    let sectionTopRight = document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
-    let sectionTopLeft = document.getElementById(unit+'NumberSectionTopLeft').style.opacity="0%";
-    let middleTop = document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
-    let middleBottom = document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
-    let sectionBottomRight = document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
-    let sectionBottomLeft = document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="0%";
-    let sectionBottom = document.getElementById(unit+'NumberSectionBottom').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopLeft').style.opacity="0%";
+    document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
+    document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionBottom').style.opacity="100%";
 }
 
 function setFour(unitTens){
     let unit= unitTens ? 'first' : 'second';
-    console.log(unit);
-    let sectionTop = document.getElementById(unit+'NumberSectionTop').style.opacity="0%";
-    let sectionTopRight = document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
-    let sectionTopLeft = document.getElementById(unit+'NumberSectionTopLeft').style.opacity="100%";
-    let middleTop = document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
-    let middleBottom = document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
-    let sectionBottomRight = document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
-    let sectionBottomLeft = document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="0%";
-    let sectionBottom = document.getElementById(unit+'NumberSectionBottom').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionTop').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopLeft').style.opacity="100%";
+    document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
+    document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionBottom').style.opacity="0%";
 }
 
 function setFive(unitTens){
     let unit= unitTens ? 'first' : 'second';
-    console.log(unit);
-    let sectionTop = document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
-    let sectionTopRight = document.getElementById(unit+'NumberSectionTopRight').style.opacity="0%";
-    let sectionTopLeft = document.getElementById(unit+'NumberSectionTopLeft').style.opacity="100%";
-    let middleTop = document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
-    let middleBottom = document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
-    let sectionBottomRight = document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
-    let sectionBottomLeft = document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="0%";
-    let sectionBottom = document.getElementById(unit+'NumberSectionBottom').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopRight').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionTopLeft').style.opacity="100%";
+    document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
+    document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionBottom').style.opacity="100%";
 }
 
 function setSix(unitTens){
     let unit= unitTens ? 'first' : 'second';
-    console.log(unit);
-    let sectionTop = document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
-    let sectionTopRight = document.getElementById(unit+'NumberSectionTopRight').style.opacity="0%";
-    let sectionTopLeft = document.getElementById(unit+'NumberSectionTopLeft').style.opacity="100%";
-    let middleTop = document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
-    let middleBottom = document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
-    let sectionBottomRight = document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
-    let sectionBottomLeft = document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="100%";
-    let sectionBottom = document.getElementById(unit+'NumberSectionBottom').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopRight').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionTopLeft').style.opacity="100%";
+    document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
+    document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottom').style.opacity="100%";
 }
 
 function setSeven(unitTens){
     let unit= unitTens ? 'first' : 'second';
-    console.log(unit);
-    let sectionTop = document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
-    let sectionTopRight = document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
-    let sectionTopLeft = document.getElementById(unit+'NumberSectionTopLeft').style.opacity="0%";
-    let middleTop = document.getElementById(unit+'NumberMiddleTop').style.opacity="0%";
-    let middleBottom = document.getElementById(unit+'NumberMiddleBottom').style.opacity="0%";
-    let sectionBottomRight = document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
-    let sectionBottomLeft = document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="0%";
-    let sectionBottom = document.getElementById(unit+'NumberSectionBottom').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopLeft').style.opacity="0%";
+    document.getElementById(unit+'NumberMiddleTop').style.opacity="0%";
+    document.getElementById(unit+'NumberMiddleBottom').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionBottom').style.opacity="0%";
 }
 
 function setEight(unitTens){
     let unit= unitTens ? 'first' : 'second';
-    console.log(unit);
-    let sectionTop = document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
-    let sectionTopRight = document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
-    let sectionTopLeft = document.getElementById(unit+'NumberSectionTopLeft').style.opacity="100%";
-    let middleTop = document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
-    let middleBottom = document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
-    let sectionBottomRight = document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
-    let sectionBottomLeft = document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="100%";
-    let sectionBottom = document.getElementById(unit+'NumberSectionBottom').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopLeft').style.opacity="100%";
+    document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
+    document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottom').style.opacity="100%";
 }
 
 function setNine(unitTens){
     let unit= unitTens ? 'first' : 'second';
-    console.log(unit);
-    let sectionTop = document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
-    let sectionTopRight = document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
-    let sectionTopLeft = document.getElementById(unit+'NumberSectionTopLeft').style.opacity="100%";
-    let middleTop = document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
-    let middleBottom = document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
-    let sectionBottomRight = document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
-    let sectionBottomLeft = document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="0%";
-    let sectionBottom = document.getElementById(unit+'NumberSectionBottom').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionTop').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionTopLeft').style.opacity="100%";
+    document.getElementById(unit+'NumberMiddleTop').style.opacity="100%";
+    document.getElementById(unit+'NumberMiddleBottom').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomRight').style.opacity="100%";
+    document.getElementById(unit+'NumberSectionBottomLeft').style.opacity="0%";
+    document.getElementById(unit+'NumberSectionBottom').style.opacity="0%";
 }
